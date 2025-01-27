c7e12500a8350dc6342d5f6cb4738d311948c3b9
pawan raju <pawankohli438@gmail.com>
2025-01-27 15:10:12 +0530

    added problem

---
+++ subArray sum.js
@@ -0,0 +1,46 @@
+/*
+subarray-sum-equals-k
+
+Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+Example 2:
+
+Input: nums = [1,2,3], k = 3
+Output: 2
+ 
+
+Constraints:
+
+1 <= nums.length <= 2 * 104
+-1000 <= nums[i] <= 1000
+-107 <= k <= 107
+*/
+
+function subarraySum(nums, k) {
+    let count = 0;
+    let prefix = 0;
+    let dictionary = { 0: 1 }; 
+
+    for (let num of nums) {
+        prefix += num;
+        
+        if ((prefix - k) in dictionary) {
+            count += dictionary[prefix - k];
+        }
+        
+        dictionary[prefix] = (dictionary[prefix] || 0) + 1;
+    }
+
+    return count;
+}
+
+
+console.log(subarraySum([1, 1, 1], 2)); // Output: 2
